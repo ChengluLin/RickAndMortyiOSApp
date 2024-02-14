cf6bea5c0e1bc0483d348c22692ff560d1b9795f
Kevin <chenglu7701@gmail.com>
2024-02-14 20:12:12 +0800

    Added InfoCell layout

--- RickAndMorty/Views/RMCharacterDetailView/RMCharacterInfoCollectionViewCell.swift
+++ RickAndMorty/Views/RMCharacterDetailView/RMCharacterInfoCollectionViewCell.swift
@@ -10,10 +10,43 @@ import UIKit
 class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifer = "RMCharacterInfoCollectionViewCell"
     
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Earth"
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Location"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "globe.americas")
+        return icon
+    }()
+    
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.addSubviews(titleContainerView, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
+        setUpConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -21,11 +54,38 @@ class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setUpConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+
+            titleLabel.leftAnchor.constraint(equalTo: titleContainerView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: titleContainerView.rightAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            
+            iconImageView.heightAnchor.constraint(equalToConstant: 70),
+            iconImageView.widthAnchor.constraint(equalToConstant: 70),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            iconImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 20),
+            iconImageView.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor, constant: -10),
+
+            valueLabel.leftAnchor.constraint(equalTo: titleContainerView.leftAnchor),
+            valueLabel.rightAnchor.constraint(equalTo: titleContainerView.rightAnchor),
+            valueLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            valueLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+
+
+            
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
