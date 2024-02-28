20d1022a40e9d277ec828052823c5169a189e63c
Kevin <chenglu7701@gmail.com>
2024-02-28 23:56:40 +0800

    Added RMLocationView

---
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -0,0 +1,20 @@
+//
+//  RMLocationView.swift
+//  RickAndMorty
+//
+//  Created by ChengLu on 2024/2/28.
+//
+
+import UIKit
+
+class RMLocationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
